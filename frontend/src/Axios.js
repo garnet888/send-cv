788153f0a8c1fc4a84895dea8a7205a8575ae4e3
garnet888 <garnet.anar19@gmail.com>
2023-10-06 16:33:17 +0800

    change axios file

--- frontend/src/Axios.js
+++ frontend/src/Axios.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:
-    process.env.DEV_TYPE === "production"
-      ? "https://send-cv-backend.vercel.app/api"
-      : "http://localhost:5000/api",
+  baseURL: process.env.AXIOS_URL || "http://localhost:5000/api",
 });
