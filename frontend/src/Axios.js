c6a2acfd9e98526f67303b6321b0be518004d0a2
garnet888 <garnet.anar19@gmail.com>
2023-10-06 17:00:23 +0800

    bk fix

--- frontend/src/Axios.js
+++ frontend/src/Axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://send-cv-backend.vercel.app/api",
+  // baseURL: "https://send-cv-backend.vercel.app/api",
+  baseURL: "http://localhost:5000/",
 });
